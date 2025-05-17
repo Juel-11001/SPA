ddbf9ad066aaa8c5cf14059a51eb1b7e629a4134
Juel-11001 <mdjewel20172017@gmail.com>
2025-05-18 03:36:31 +0600

    working with middleware and computed data conditional rendering (Display flash message)

--- resources/js/Layouts/mainLayout.vue
+++ resources/js/Layouts/mainLayout.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="">
-      <b>The page with Time : {{ timer }}</b>
+    <div  v-if="flashSuccess" class="_success">
+      {{ flashSuccess }}
+      <!-- <b>The page with Time : {{ timer }}</b> -->
       <br>
     </div>
   <Link href="/Training/Course/Udemy/SPA/public/">Home Page</Link>&nbsp;
@@ -9,8 +10,22 @@
 </template>
 
 <script setup>
-  import {Link} from '@inertiajs/vue3'
-  import { ref } from 'vue'
-  const timer = ref(0)
-  setInterval(()=>timer.value++,1000)
+  import {Link, usePage} from '@inertiajs/vue3'
+import { computed } from 'vue';
+
+    //page.props.value.flash.success
+  const page=usePage()
+  const flashSuccess=computed(()=>page.props.flash.success)
+  // import { ref } from 'vue'
+  // const timer = ref(0)
+  // setInterval(()=>timer.value++,1000)
 </script> 
+<style scoped>
+  ._success{
+    background-color: green;
+    color: black;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    padding: 10px;
+    border-radius: 10px;
+  }
+</style>
