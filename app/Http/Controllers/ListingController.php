49a840a9e5253a25e52b803d28533f0b94024ff2
Juel-11001 <mdjewel20172017@gmail.com>
2025-05-25 04:50:16 +0600

    working with Local query Scopes in Eloquent and Filtering usering local query scope

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -24,41 +24,12 @@ class ListingController extends Controller
         $filters=$request->only([
             'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
         ]);
-        // $query=Listing::orderByDesc('created_at');
-        // if($filters['priceFrom'] ?? false){
-        //     $query->where('price', '>=', $filters['priceFrom']);
-        // }
-        // if($filters['priceTo'] ?? false){
-        //     $query->where('price', '<=', $filters['priceTo']);
-        // }
-        // if($filters['beds'] ?? false){
-        //     $query->where('beds',$filters['beds']);
-        // }
-        // if($filters['baths'] ?? false){
-        //     $query->where('bath',$filters['baths']);
-        // }
-        // if($filters['areaFrom'] ?? false){
-        //     $query->where('area', '>=', $filters['areaFrom']);
-        // }
-        // if($filters['areaTo'] ?? false){
-        //     $query->where('area', '<=', $filters['areaTo']);
-        // }
         return inertia('listing/index', [
             'filters'=>$filters,
-            'listings' => Listing::orderByDesc('created_at')
-            ->when($filters['priceFrom'] ?? false,
-                fn($query, $value) => $query->where('price', '>=', $value))
-            ->when($filters['priceTo'] ?? false,
-                fn($query, $value) => $query->where('price', '<=', $value))
-            ->when($filters['beds'] ?? false,
-                fn($query, $value) => $query->where('beds',(int) $value< 6 ? '=' : '>=', $value))
-            ->when($filters['baths'] ?? false,
-                fn($query, $value) => $query->where('bath',(int) $value < 6 ? '=' : '>=', $value))
-            ->when($filters['areaFrom'] ?? false,
-                fn($query, $value) => $query->where('area', '>=', $value))    
-            ->when($filters['areaTo'] ?? false,
-                fn($query, $value) => $query->where('area', '<=', $value))
-            ->paginate(10)->withQueryString()
+            'listings' => Listing::mostRecent()
+                ->filter($filters)
+                ->paginate(10)
+                ->withQueryString()
         ]);
     }
 
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -22,4 +23,24 @@ class Listing extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+        public function scopeMostRecent(Builder $query) : Builder
+    {
+        return $query->orderBy('created_at', 'desc');        
+    }
+    public function scopeFilter(Builder $query, array $filters) : Builder
+    {
+        return $query->when($filters['priceFrom'] ?? false,
+                fn($query, $value) => $query->where('price', '>=', $value))
+            ->when($filters['priceTo'] ?? false,
+                fn($query, $value) => $query->where('price', '<=', $value))
+            ->when($filters['beds'] ?? false,
+                fn($query, $value) => $query->where('beds',(int) $value< 6 ? '=' : '>=', $value))
+            ->when($filters['baths'] ?? false,
+                fn($query, $value) => $query->where('bath',(int) $value < 6 ? '=' : '>=', $value))
+            ->when($filters['areaFrom'] ?? false,
+                fn($query, $value) => $query->where('area', '>=', $value))    
+            ->when($filters['areaTo'] ?? false,
+                fn($query, $value) => $query->where('area', '<=', $value));
+    }
+    
 }
